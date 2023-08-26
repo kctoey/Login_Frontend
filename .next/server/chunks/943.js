c4f41301446c8e86c5ac998be868825dabd06c90
kctoey <toeyknchprn@gmail.com>
2023-08-26 14:11:24 +0700

    update

---
+++ .next/server/chunks/943.js
@@ -0,0 +1,376 @@
+exports.id = 943;
+exports.ids = [943];
+exports.modules = {
+
+/***/ 66887:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 85958))
+
+/***/ }),
+
+/***/ 52625:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__),
+/* harmony export */   getServerSideProps: () => (/* binding */ getServerSideProps)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(17421);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_mui_material__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(93258);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(18038);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _context_UserContext__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(85958);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(57114);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_navigation__WEBPACK_IMPORTED_MODULE_3__);
+/* __next_internal_client_entry_do_not_use__ getServerSideProps,default auto */ 
+
+
+
+
+
+function Page() {
+    const [email, setEmail] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    const [password, setPassword] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    const [message, setMessage] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(null);
+    const { username, setUsername, setToken } = (0,react__WEBPACK_IMPORTED_MODULE_1__.useContext)(_context_UserContext__WEBPACK_IMPORTED_MODULE_2__.AppContext);
+    const Router = (0,next_navigation__WEBPACK_IMPORTED_MODULE_3__.useRouter)();
+    const handleClick2 = ()=>{
+        Router.push("/register");
+    };
+    const handleSubmit1 = (e)=>{
+        e.preventDefault();
+        const headers = {
+            "Content-type": "application/json"
+        };
+        axios__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z.post("http://localhost:8080/user/signin", {
+            email,
+            password
+        }, {
+            headers
+        }).then((res)=>{
+            setMessage(res.data.message);
+            if (res.data.status === "ok") {
+                setUsername(res.data.data[0].name);
+                setToken(res.data.token);
+                Router.push("/userdata");
+            }
+        }).catch((err)=>{
+            console.log(err);
+        });
+    };
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        className: "font-Monstserrat bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100  flex flex-col justify-center text-center w-screen h-screen ",
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: " bg-slate-200 max-w-[500px] w-full m-auto rounded-3xl shadow-xl p-4",
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h1", {
+                    className: "p-4 text-3xl  ",
+                    children: "Login"
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("form", {
+                    className: "p-4",
+                    onSubmit: handleSubmit1,
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: "py-4",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_5__.TextField, {
+                                required: true,
+                                type: "email",
+                                id: "outlined-basic",
+                                label: "Email",
+                                variant: "outlined",
+                                value: email,
+                                onChange: (e)=>setEmail(e.target.value)
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                            className: "py-4",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_5__.TextField, {
+                                type: "password",
+                                required: true,
+                                id: "outlined-basic",
+                                label: "Password",
+                                variant: "outlined",
+                                value: password,
+                                onChange: (e)=>setPassword(e.target.value)
+                            })
+                        }),
+                        /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                            className: "pt-4",
+                            children: [
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_5__.Button, {
+                                    variant: "outlined",
+                                    size: "medium",
+                                    type: "submit",
+                                    children: "Login"
+                                }),
+                                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("h1", {
+                                    className: "pt-4",
+                                    children: [
+                                        " ",
+                                        message
+                                    ]
+                                })
+                            ]
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("p", {
+                    children: [
+                        "Don't have account",
+                        " ",
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                            className: "text-blue-500 font-bold cursor-pointer hover:underline",
+                            onClick: handleClick2,
+                            children: "Register"
+                        })
+                    ]
+                })
+            ]
+        })
+    });
+}
+async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await handleSubmit;
+    const data = await res.json();
+    // Pass data to the page via props
+    return {
+        props: {
+            data
+        }
+    };
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Page);
+
+
+/***/ }),
+
+/***/ 24066:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Page)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(18038);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(17421);
+/* harmony import */ var _mui_material__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_mui_material__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(57114);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_navigation__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(93258);
+// path: /pages/login.js
+/* __next_internal_client_entry_do_not_use__ default auto */ 
+
+
+
+// Change import from 'axios' to 'axios.config.js'
+
+function Page() {
+    const [email, setEmail] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    const [password, setPassword] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    const [message, setMessage] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    const [userName, setuserName] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(null);
+    const [name, setName] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    const [userEmail, setuserEmail] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(null);
+    const Router = (0,next_navigation__WEBPACK_IMPORTED_MODULE_2__.useRouter)();
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const headers = {
+            "Content-type": "application/json"
+        };
+        axios__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z.post("http://localhost:8080/user/signup", {
+            name,
+            email,
+            password
+        }, {
+            headers
+        }).then((res)=>{
+            setMessage(res.data.message);
+            Router.push("/login");
+        }).catch((err)=>{
+            alert("cannot signup");
+        });
+    // const body = JSON.stringify({
+    //   email: email.current.value,
+    //   password: password.current.value,
+    // });
+    // const res = await axios.post("/user/login", body);
+    // console.log(res.data);
+    };
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        className: "font-Monstserrat bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100  flex flex-col justify-center text-center w-screen h-screen ",
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: " bg-slate-200 max-w-[500px] w-full m-auto rounded-3xl shadow-xl p-4",
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h1", {
+                    className: "p-4 text-3xl  ",
+                    children: "Register Page"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("form", {
+                    className: "p-4",
+                    onSubmit: handleSubmit,
+                    children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex flex-col justify-center items-center space-y-4",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_4__.TextField, {
+                                variant: "outlined",
+                                required: true,
+                                type: "text",
+                                label: "name",
+                                value: name,
+                                onChange: (e)=>setName(e.target.value)
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_4__.TextField, {
+                                variant: "outlined",
+                                required: true,
+                                type: "email",
+                                label: "email",
+                                value: email,
+                                onChange: (e)=>setEmail(e.target.value)
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_4__.TextField, {
+                                variant: "outlined",
+                                required: true,
+                                type: "password",
+                                label: "password",
+                                value: password,
+                                onChange: (e)=>setPassword(e.target.value)
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_mui_material__WEBPACK_IMPORTED_MODULE_4__.Button, {
+                                variant: "outlined",
+                                size: "medium",
+                                type: "submit",
+                                children: "Submit"
+                            })
+                        ]
+                    })
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+                    children: message
+                })
+            ]
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 85958:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   AppContext: () => (/* binding */ AppContext),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(18038);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _app_login_page__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(52625);
+/* harmony import */ var _app_register_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(24066);
+/* __next_internal_client_entry_do_not_use__ AppContext,default auto */ 
+
+
+
+const AppContext = /*#__PURE__*/ (0,react__WEBPACK_IMPORTED_MODULE_1__.createContext)({});
+const ContextCase1 = ({ children })=>{
+    const [username, setUsername] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(null);
+    const [token, setToken] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(null);
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(AppContext.Provider, {
+        value: {
+            username,
+            setUsername,
+            token,
+            setToken
+        },
+        children: children
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ContextCase1);
+
+
+/***/ }),
+
+/***/ 59240:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ RootLayout),
+  metadata: () => (/* binding */ metadata)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(56786);
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-flight-loader/module-proxy.js
+var module_proxy = __webpack_require__(61363);
+;// CONCATENATED MODULE: ./context/UserContext.js
+
+const proxy = (0,module_proxy.createProxy)(String.raw`E:\Front end\LoginProject_Frontend\context\UserContext.js`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule, $$typeof } = proxy;
+const __default__ = proxy.default;
+
+const e0 = proxy["AppContext"];
+
+
+/* harmony default export */ const UserContext = (__default__);
+// EXTERNAL MODULE: ./app/globals.css
+var globals = __webpack_require__(67272);
+;// CONCATENATED MODULE: ./app/layout.js
+// These styles apply to every route in the application
+
+
+
+const metadata = {
+    title: "Create Next App",
+    description: "Generated by create next app"
+};
+function RootLayout({ children }) {
+    return /*#__PURE__*/ jsx_runtime_.jsx("html", {
+        lang: "en",
+        children: /*#__PURE__*/ jsx_runtime_.jsx("body", {
+            children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)(UserContext, {
+                children: [
+                    children,
+                    " "
+                ]
+            })
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 67272:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
